f26c5468f4ce071d0e78f1daf689e97db0bbf561
StruggleYang <yq1724555319@gmail.com>
2018-03-05 11:47:06 +0800

    update

--- src/main/java/com/today/event/consumer/EventConsumer.java
+++ src/main/java/com/today/event/consumer/EventConsumer.java
@@ -12,23 +12,20 @@ import org.slf4j.LoggerFactory;
  * Create by 2018/3/3 20:39
  * email :yq1724555319@gmail.com
  */
-
+@KafkaListener(groupId = "eventConsumer", topic = "user_1.0.0_event",kafkaHostKey="kafka.consumer.host")
 public class EventConsumer {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-    @KafkaListener(groupId = "eventConsumer", topic = "user_1.0.0_event",kafkaHostKey="kafka.consumer.host",
-            serializer = "com.github.dapeng.user.scala.events.serializer.RegisteredEventSerializer")
+    @KafkaListener(serializer = "com.github.dapeng.user.scala.events.serializer.RegisteredEventSerializer")
     public void subscribeRegisteredEvent(RegisteredEvent event){
         LOGGER.info("Subscribed RegisteredEvent ==> {}",event.toString());
     }
 
-    @KafkaListener(groupId = "eventConsumer", topic = "user_1.0.0_event",kafkaHostKey="kafka.consumer.host",
-            serializer = "com.github.dapeng.user.scala.events.serializer.ActivedEventSerializer")
+    @KafkaListener(serializer = "com.github.dapeng.user.scala.events.serializer.ActivedEventSerializer")
     public void subscribeActivedEvent(ActivedEvent event){
         LOGGER.info("Subscribed RegisteredEvent ==> {}",event.toString());
     }
 
-    @KafkaListener(groupId = "eventConsumer", topic = "user_1.0.0_event",kafkaHostKey="kafka.consumer.host",
-            serializer = "com.github.dapeng.user.scala.events.serializer.BlackedEventSerializer")
+    @KafkaListener(serializer = "com.github.dapeng.user.scala.events.serializer.BlackedEventSerializer")
     public void subscribeBlackedEvent(BlackedEvent event){
         LOGGER.info("Subscribed RegisteredEvent ==> {}",event.toString());
     }
