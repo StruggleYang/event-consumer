1279bf57b219fd073d4ef706c7102c4966cf34d1
StruggleYang <yq1724555319@gmail.com>
2018-03-05 17:54:57 +0800

    代码调整

--- src/main/java/com/today/event/consumer/EventConsumer.java
+++ src/main/java/com/today/event/consumer/EventConsumer.java
@@ -3,6 +3,10 @@ package com.today.event.consumer;
 import com.github.dapeng.user.scala.events.ActivedEvent;
 import com.github.dapeng.user.scala.events.BlackedEvent;
 import com.github.dapeng.user.scala.events.RegisteredEvent;
+import com.github.dapeng.user.scala.events.serializer.ActivedEventSerializer;
+import com.github.dapeng.user.scala.events.serializer.BlackedEventSerializer;
+import com.github.dapeng.user.scala.events.serializer.RegisteredEventSerializer;
+import com.today.eventbus.annotation.KafkaConsumer;
 import com.today.eventbus.annotation.KafkaListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,21 +16,22 @@ import org.slf4j.LoggerFactory;
  * Create by 2018/3/3 20:39
  * email :yq1724555319@gmail.com
  */
-@KafkaListener(groupId = "eventConsumer", topic = "user_1.0.0_event",kafkaHostKey="kafka.consumer.host")
+@KafkaConsumer(groupId = "eventConsumer1", topic = "user_1.0.0_event")
 public class EventConsumer {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-    @KafkaListener(serializer = "com.github.dapeng.user.scala.events.serializer.RegisteredEventSerializer")
-    public void subscribeRegisteredEvent(RegisteredEvent event){
-        LOGGER.info("Subscribed RegisteredEvent ==> {}",event.toString());
+
+    @KafkaListener(serializer = RegisteredEventSerializer.class)
+    public void subscribeRegisteredEvent(RegisteredEvent event) {
+        LOGGER.info("subscribeRegisteredEvent方法=========> 订阅到了消息 RegisteredEvent ==> {}", event.toString());
     }
 
-    @KafkaListener(serializer = "com.github.dapeng.user.scala.events.serializer.ActivedEventSerializer")
-    public void subscribeActivedEvent(ActivedEvent event){
-        LOGGER.info("Subscribed RegisteredEvent ==> {}",event.toString());
+    @KafkaListener(serializer = ActivedEventSerializer.class)
+    public void subscribeActivedEvent(ActivedEvent event) {
+        LOGGER.info("subscribeActivedEvent方法=========> 订阅到了消息  RegisteredEvent ==> {}", event.toString());
     }
 
-    @KafkaListener(serializer = "com.github.dapeng.user.scala.events.serializer.BlackedEventSerializer")
-    public void subscribeBlackedEvent(BlackedEvent event){
-        LOGGER.info("Subscribed RegisteredEvent ==> {}",event.toString());
+    @KafkaListener(serializer = BlackedEventSerializer.class)
+    public void subscribeBlackedEvent(BlackedEvent event) {
+        LOGGER.info("subscribeBlackedEvent方法=========> 订阅到了消息  RegisteredEvent ==> {}", event.toString());
     }
 }
